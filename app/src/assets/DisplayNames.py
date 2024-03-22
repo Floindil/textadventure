ce642ff81988a8565705d334d49f592267e6c722
Celin Villars <cvillars@arttence.com>
2024-03-22 15:30:38 +0100

    remove old corpses

---
+++ app/src/assets/DisplayNames.py
@@ -0,0 +1,57 @@
+class PLAYER:
+    CHARACTER = 'Character'
+    ITEMS = 'Items'
+    EQUIPMENT = 'Equipment'
+    ATTRIBUTES = 'Attributes'
+    STATISTICS = 'Statistics'
+    RESOURCES = 'Resources'
+    BUFFS = 'Buffs'
+
+class CHARACTER:
+    NAME = 'Name'
+    RACE = 'Race'
+    BIOLOGY = 'Biology'
+
+class RESOURCES:
+    CURRENCY = 'Currency'
+    EXPERIENCE = 'Experience'
+
+class ITEMS:
+    CONSUMABLE = 'Consumable'
+    WEAPON = 'Weapon'
+    ARMOR = 'Armor'
+    TALISMAN = 'Talisman'
+    KEYITEM = 'Keyitem'
+    MATERIAL = 'Material'
+
+class EQUIPMENT:
+    ONHAND = 'On Hand'
+    OFFHAND = 'Off Hand'
+    ARMOR = 'Armor'
+    TALISMAN = 'Talisman'
+
+class EQUIPMENTVALUES:
+    ARMOR = 'Armor'
+    ATTACK = 'Attack'
+    BLOCK = 'Block'
+    RANGE = 'Range'
+    ACCURACY = 'Accuracy'
+
+class ATTRIBUTES:
+    VIGOR = 'Vigor'
+    AGILITY = 'Agility'
+    STRENGHT = 'Strenght'
+    DEXTERITY = 'Dexterity'
+    INTELLIGENCE = 'Intelligence'
+    LIGHTMAGIC = 'Light Magic'
+    DARKMAGIC = 'Dark Magic'
+
+class PROPERTY:
+    HEALTH = 'Health'
+    STAMINA = 'Stamina'
+    MANA = 'Mana'
+
+class PROPERTYBASE:
+    HEALTH = 15
+    STAMINA = 10
+    MANA = 10
--- app/src/assets/items/others/keyitems.py
+++
@@ -1,9 +0,0 @@
-from ...names_values import items as i
-from .basic.item import Item
-'''
-Values imported from names_values file are only stated there to allow easy name changes late in the project
-'''
-class Keyitems(Item):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.type= i[4]
